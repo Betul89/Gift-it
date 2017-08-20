e052278fe72c46806e40a1654692c927bba60a87
Betul89 <31154637+Betul89@users.noreply.github.com>
2017-08-20 10:58:08 +0300

    İlk oluşturma

---
+++ app/src/main/java/com/giftit/betuldizdaroglu/giftit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.giftit.betuldizdaroglu.giftit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
